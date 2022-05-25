09245648f0923c1af43214681631e61794b1bf2b
Lucas Magno <lucasmagno695@gmail.com>
2022-05-25 14:58:12 -0300

    Projeto Spring

---
+++ wantfood-api/src/main/java/com/wantfood/aplication/WantfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.wantfood.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantfoodApiApplication.class, args);
+	}
+
+}
---
+++ wantfood-api/src/test/java/com/wantfood/aplication/WantfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wantfood.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
