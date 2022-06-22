10ab5998d28f9b6398b81294d2eb5509b98a81bf
Lucas Magno <lucasmagno695@gmail.com>
2022-06-22 01:37:34 -0300

    adicionado novos models, relacionando e criando novas tabelas com o flyway

---
+++ wantfood-api/src/main/java/com/wantfood/aplication/domain/model/ItemPedido.java
@@ -0,0 +1,40 @@
+package com.wantfood.aplication.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer quantidade;
+	
+	private BigDecimal precoUnitario;
+	
+	private BigDecimal precoTotal;
+	
+	private String observacao;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+}
---
+++ wantfood-api/src/main/java/com/wantfood/aplication/domain/model/StatusPedido.java
@@ -0,0 +1,8 @@
+package com.wantfood.aplication.domain.model;
+
+public enum StatusPedido {
+	CRIADO,
+	CONFIRMADO,
+	ENTREGUE,
+	CANCELADO
+}
