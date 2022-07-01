e73c929f94e0570a9536fdcb0fff7e5577750bf8
Magno <lucasmagno695@gmail.com>
2022-06-30 22:35:27 -0300

    Implementacao do pacote exceptionhandler

--- wantfood-api/src/main/java/com/wantfood/aplication/api/exceptionhandler/ApiExceptionHandler.java
+++ wantfood-api/src/main/java/com/wantfood/aplication/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.wantfood.aplication.api.exceptionhandler;
 
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.wantfood.aplication.domain.exception.EntidadeEmUsoException;
 import com.wantfood.aplication.domain.exception.EntidadeNaoEncontradaException;
 import com.wantfood.aplication.domain.exception.NegocioException;
@@ -19,8 +24,59 @@ import com.wantfood.aplication.domain.exception.NegocioException;
 @ControllerAdvice 
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {	
+		/*
+		 * Pegando a raiz do problema para ser passado como o erro pro usuário,
+		 * afim de especificar melhor a mensagem de erro 
+		 * Pegando a causa raiz
+		 * */
+		Throwable rootCause = ExceptionUtils.getRootCause(e);
+		
+		/*
+		 *Fazendo a verificação, se a causa raiz(rootCause) for uma InvalidFormatException então
+		 * chamará o metodo que irá tratar de forma especifica
+		 * */
+		if(rootCause instanceof InvalidFormatException) {
+			return handleInvalidFormatException((InvalidFormatException) rootCause, headers,
+					status, request);
+		}
+		
+		ProblemType problemType = ProblemType.ERRO_NA_MENSAGEM;
+		String detail = "O corpo está requisição está inválido. Possivel erro na sintaxe.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(e, problem, headers, status, request);
+	}
+	
 	/*
-	 * Criando um metodo para controlar os consoles de erro
+	 * Método a ser chamado se a causa raiz for um InvalidFormatException
+	 * */
+	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException e,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		/*
+		 * Criando variável que irá pegar o Path da exception
+		 *  e irá formatar o nome dela, delimitando por um '.'
+		 * */
+		String path = e.getPath().stream()
+				.map(ref -> ref.getFieldName())
+				.collect(Collectors.joining("."));
+		
+		ProblemType problemType = ProblemType.ERRO_NA_MENSAGEM;
+		String detail = String.format("A propriedade '%s' recebeu o valor '%s',"
+				+ " que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+				path, e.getValue(), e.getTargetType().getSimpleName());
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(e, problem, headers, status, request);
+	}
+
+	/*
+	 * Criando metodo para controlar os consoles de erro
 	 * @ExceptionHandler Aceita mais de um argumento, pode ser usado {EstadoNaoEncontradoException.class},
 	 * {EstadoeXEMPLOException.class}
 	 * */
--- wantfood-api/src/main/java/com/wantfood/aplication/api/exceptionhandler/ProblemType.java
+++ wantfood-api/src/main/java/com/wantfood/aplication/api/exceptionhandler/ProblemType.java
@@ -9,7 +9,8 @@ public enum ProblemType {
 	 * */
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	ERRO_NA_MENSAGEM("/erro-na-mensagem","Erro na mensagem");
 	
 	private String title;
 	private String uri;
